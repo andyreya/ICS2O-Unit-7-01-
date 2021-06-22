a5a6a50e3cb6fc35c7429555f0a263e4a235176e
andyreya <83035508+andyreya@users.noreply.github.com>
2021-06-22 08:09:55 +0000

    spaces

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 /* Connects the 'Guess' button to the 'random' function */
-document.getElementById('button').addEventListener('click',randomizer)
+document.getElementById('button').addEventListener('click', randomizer)
 let userInput = 0
 let randomNumber = 0
 
 function randomizer () {
   /* converts number typed in by user to an integer */
-  userInput = document.getElementById('input').value 
+  userInput = document.getElementById('input').value
   userInput = parseInt(userInput)
   /* generate a random number between 1 and 6 */
   randomNumber = (Math.random() * 6) + 1
   /* compare the two variables after the users input */
-  if (randomNumber == userInput) {
+  if (randomNumber === userInput) {
     document.getElementById('answer').innerHTML = 'You win !'
-  } else (randomNumber != userInput) 
-    document.getElementById('answer').innerHTML = 'Try again'
+  } else (randomNumber !== userInput)
+  document.getElementById('answer').innerHTML = 'Try again'
 }
